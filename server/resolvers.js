1d7404057dc1a0141385ba17dfe70eca18657f8d
Erasmo Hernández <erasmoh@gmail.com>
2022-02-12 18:24:13 -0300

    Packages updated to match course and Jobs/Company objects are working

---
+++ server/resolvers.js
@@ -0,0 +1,12 @@
+const db = require("./db");
+
+const Query = {
+  //   greeting: () => "Hello world",
+  jobs: () => db.jobs.list(),
+};
+
+const Job = {
+  company: (job) => db.companies.get(job.companyId),
+};
+
+module.exports = { Query, Job };
